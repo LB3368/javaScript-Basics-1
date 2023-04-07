413778e5059703917296962d81affbc2c688d96b
Latisha Bridges <msbridges3368@gmail.com>
2023-04-07 17:35:19 -0400

    first commit

---
+++ gitInfo.js
@@ -0,0 +1,80 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = `Git is like a big toy box for your computer where you can save all your toys (also known as files) and keep track of every change you make to them. You can play with your toys and put them back in the toy box whenever you want. And if you make a mistake or want to go back to how your toys were before, Git lets you do that too! It's like a magical toy box that remembers everything you do with your toys, so you can always go back to any version you want.`
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = `GitHub is a place on the internet where you can show your toys (also known as code) to other people and they can show you their toys too. It's like a big playground where everyone can share their toys and play together. You can look at other people's toys and even borrow them to play with or add new things to them. And just like a playground, you can also talk to your friends and share your ideas about your toys. So, GitHub is like a fun place on the internet where you can share your toys with your friends and play together.`
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = `When you want to start playing with your toys (also known as files) in Git, you need to create a special toy box first. This is what git init does. It's like creating a new toy box just for your Git toys. Once you create this toy box, you can start putting your toys (files) in it and Git will keep track of all the changes you make to them. It's like giving your toys a special place to live in, where they can be protected and watched over by Git.`
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = `Imagine that your friend has a really cool toy that you really want to play with. But your friend lives far away and you can't go over to their house to play with it. That's where git clone comes in! It's like your friend is mailing you a copy of their toy. You can use git clone to make a copy of your friend's toy (also known as code) and keep it on your computer so you can play with it anytime you want! Now you have your very own copy of your friend's toy to play with, just like they have their own copy. And if your friend makes any changes to their toy, you can use git pull to get the updated version and keep playing with the latest version of their toy.`
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = `When you're playing with your toys (also known as files) in Git, it's important to know which toys you've changed and which ones you haven't. That's where git status comes in! It's like a helper that tells you which toys you've played with and which ones you haven't. It's like looking at a list of all your toys and seeing which ones you've used recently. That way, you can make sure you don't forget about any of your toys and keep track of all the changes you make to them. With git status, you'll always know what you've been playing with and what you need to do next.`
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = `When you're playing with your toys (also known as files) in Git, sometimes you make changes to them that you want to keep track of. That's where git add comes in! It's like telling Git which toys you've changed and want to keep track of. It's like putting a toy in a special box and saying "Hey, I made changes to this toy and I want to keep track of them!" That way, Git knows which toys to keep an eye on and which ones you're not making changes to right now. With git add, you can tell Git which toys you want to keep track of and make sure all your changes are saved and remembered.`
+
+let gitAddCode = `git add .`
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = `When you're playing with your toys (also known as files) in Git, you sometimes make a lot of changes to them over time. It can be hard to remember exactly what you changed and when you changed it! That's where git commit comes in. It's like telling Git to take a picture of all the toys you changed and write down what you did to them. It's like saying "Hey, Git, I made a lot of changes to my toys and I want you to remember all of them!" When you do this, Git takes a snapshot of your toys and saves a record of all the changes you made to them. This way, you can always go back in time and see what your toys looked like at any point in the past. With git commit, you can make sure that all your changes are saved and remembered forever.`
+
+let gitCommitCode = `git commit -m "commit message"`
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = `When you're playing with your toys (also known as files) in Git, sometimes you want to share them with your friends or family. That's where git push comes in! It's like sending your special toy box (also known as your repository) to your friends so they can play with your toys too. It's like saying "Hey, friends, I made some really cool changes to my toys and I want you to see them too!" When you use git push, Git sends all the changes you made to your toys to a special place where your friends can see them too. That way, you and your friends can all play with the same toys and see all the cool changes you made. With git push, you can share your toys and your creativity with the world!.`
---
+++ sdlc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+console.log(`\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\`)
+console.log(`The answer is: Planning
+Analysis of Requirements
+Design
+Implementation
+Testing & Integration
+Maintain`)
+console.log(`\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\`)
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = `The second stage of the SDLC where the project team analyzes and documents the requirements of the software system, including the functional and non-functional requirements, technical specifications, and user expectations.`
+
+const design = `The third stage of the SDLC where the software system's architecture and design are developed based on the requirements identified in the previous stage. This includes creating a high-level design, detailed design, and choosing the technology stack.`
+
+const implementation = `The fourth stage of the SDLC where the software system is developed, including coding, testing, debugging, and integration. This stage involves converting the design into a working software system.`
+
+const testingIntegration = `The fifth stage of the SDLC where the software system is tested to ensure that it meets the requirements, is error-free, and integrates well with other systems. This stage includes testing at various levels, including unit testing, integration testing, system testing, and acceptance testing.`
+
+const maintain = `The final stage of the SDLC where the software system is deployed to production, and ongoing maintenance, including bug fixes, enhancements, and updates, are performed. This stage ensures that the software system remains effective and efficient over time.`
